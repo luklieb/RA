f34eb8153dcc0cdc5986bc0f77380141a915f636
Lukas L <Lukas@example.com>
2016-11-02 12:20:00 +0100

    gnuplot skript ferti

--- ex01/gen_samples.sh
+++ ex01/gen_samples.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-START=$((16*1023))
-STOP=$((16*1021*1027))
+START=$((128*1024))
+STOP=$((128*1024*1024))
 SAMPLES=60
 
 OUTFILE=input_sizes.txt
@@ -9,14 +9,19 @@ OUTFILE=input_sizes.txt
 rm -f $OUTFILE
 
 y=`echo "e(l($STOP/$START)/$SAMPLES)" | bc -l`
-cd /Users/lukas/Documents/Universitaet/Studium/WS\ 16-17/Rechnerarchitektur/Rechnerübung/ex01
-for i in `seq 0 $SAMPLES`
+#cd /Users/lukas/Documents/Universitaet/Studium/WS\ 16-17/Rechnerarchitektur/Rechnerübung/ex01
+for j in 2 3 4 8
 do
-	kb=`echo "$START*$y^$i" | bc -l | xargs printf "%d\n"`
-	bla=$(./main $kb)
-	echo "$kb, $bla"  >> $OUTFILE
-done
+	echo "# $(j)-fach unrolled: "
+	for i in `seq 0 $SAMPLES`
+	do
+		kb=`echo "$START*$y^$i" | bc -l | xargs printf "%d\n"`
+		bla=$(./main $kb $j)
+		echo "$kb, $bla"  >> $OUTFILE
+	done
+	echo " "
+	done
 
 TMP=`mktemp`
 uniq $OUTFILE >> $TMP
-mv $TMP $OUTFILE
+mv $TMP $OUTFILE 
